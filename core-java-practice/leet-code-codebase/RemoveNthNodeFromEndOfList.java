896f029856e5fb40d19fa647f0b5057609689873
shivamGupta730 <144233987+shivamGupta730@users.noreply.github.com>
2025-12-17 23:38:32 +0530

    "[shivam gupta] remove nth from end"

---
+++ core-java-practice/leet-code-codebase/RemoveNthNodeFromEndOfList.java
@@ -0,0 +1,28 @@
+/**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode() {}
+ *     ListNode(int val) { this.val = val; }
+ *     ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+ * }
+ */
+class Solution {
+    public ListNode removeNthFromEnd(ListNode head, int n) {
+        int count=0;
+        ListNode temp=head;
+        while(temp!=null){
+            count++;
+            temp=temp.next;
+        }
+        int pos=count-n; // pahele puri length find karo then usme se n minus kardo toh start se position pta chl jayegi
+          if(pos==0) return head=head.next;
+          ListNode curr=head;
+          for(int i=0;i<pos-1;i++){
+            curr=curr.next;
+          }
+          curr.next=curr.next.next;
+           return head;
+    }
+}
