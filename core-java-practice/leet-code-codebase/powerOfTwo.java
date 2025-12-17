249f80e6c52af6fae6c9bc1aff97b14c4276aa75
shivamGupta730 <144233987+shivamGupta730@users.noreply.github.com>
2025-12-17 15:03:00 +0530

    "[shivam gupta] power of two"

--- core-java-practice/leet-code-codebase/powerOfTwo.java
+++ core-java-practice/leet-code-codebase/powerOfTwo.java
@@ -1,9 +1,15 @@
 class Solution {
-    //  power of two question
+
+    // Checking if a number is power of two
     public boolean isPowerOfTwo(int n) {
+        
+        // 1 is a power of two (2^0), so return true
         if(n == 1) return true;
+
+        // 0 can never be a power of two
         if(n == 0) return false;
-        return n%2 == 0 ? isPowerOfTwo(n/2) : false;
+
+        // If divisible by 2, keep dividing and check again, else it's not power of two
+        return n % 2 == 0 ? isPowerOfTwo(n / 2) : false;
     }
-    
 }
