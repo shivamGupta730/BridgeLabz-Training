32e44a3d9d9903e33526e3e062b7b371de1f8127
shivamGupta730 <144233987+shivamGupta730@users.noreply.github.com>
2025-12-17 22:41:23 +0530

    "[shivam gupta] Celsius to fahrenheit"

---
+++ core-java-practice/gcr-codebase/programming-elements/CelsiusToFahrenheit.java
@@ -0,0 +1,9 @@
+import java.util.*;
+
+public class CelsiusToFahrenheit {
+    public static void main(String[] args) {
+        Scanner sc= new Scanner(System.in);
+        float celsius=sc.nextFloat();
+        System.out.println((celsius*9/5)+32);
+    }
+}
