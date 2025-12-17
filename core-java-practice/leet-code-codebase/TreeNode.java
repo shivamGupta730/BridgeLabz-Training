1623faa87be1884fb3230b05dbb99bc92dc88297
shivamGupta730 <144233987+shivamGupta730@users.noreply.github.com>
2025-12-17 23:54:21 +0530

    "[shivam gupta] preorder traversal"

---
+++ core-java-practice/leet-code-codebase/TreeNode.java
@@ -0,0 +1,30 @@
+import java.util.*;
+
+public class TreeNode {
+    int val;
+     TreeNode left;
+    TreeNode right;
+    TreeNode() {}
+    TreeNode(int val) { this.val = val; }
+    TreeNode(int val, TreeNode left, TreeNode right) {
+         this.val = val;
+        this.left = left;
+          this.right = right;
+    }
+ }
+class Solution {
+    List<Integer> preorderTraverse(TreeNode root,List<Integer> list) {
+
+        if(root==null)
+            return list;
+        list.add(root.val);
+        preorderTraverse(root.left,list);
+        preorderTraverse(root.right,list);
+        return list;
+    }
+    public List<Integer> preorderTraversal(TreeNode root) {
+        List<Integer> list = new ArrayList<Integer>();
+        list = preorderTraverse(root,list);
+        return list;
+    }
+}
