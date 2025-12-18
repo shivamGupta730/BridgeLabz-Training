2b4d342fbadcfb3c5b3f2b4f2889985fbf32dcdf
shivamGupta730 <144233987+shivamGupta730@users.noreply.github.com>
2025-12-18 15:11:11 +0530

    "[shivam gupta] commit"

--- core-csharp-practice/gcr-codebase/programming-elements/PowerCalculation.cs
+++ core-csharp-practice/gcr-codebase/programming-elements/PowerCalculation.cs
@@ -1,9 +1,10 @@
 using System;
 class PowerCalculation {
    static void Main() {
-      int base=2;
-        int exponent=3;
-        Console.WriteLine(Math.pow(base, exponent));
-       
-    }
+      int b = 2;
+      int exponent = 3;
+
+      double result = Math.Pow(b, exponent);
+      Console.WriteLine(result);
+   }
 }
