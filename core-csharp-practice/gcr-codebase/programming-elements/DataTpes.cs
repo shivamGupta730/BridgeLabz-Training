46339abd2e074246ae355ceaa664c584e04e52ed
shivamGupta730 <144233987+shivamGupta730@users.noreply.github.com>
2025-12-19 11:29:54 +0530

    "[shivam gupta] DataTypes "

---
+++ core-csharp-practice/gcr-codebase/programming-elements/DataTpes.cs
@@ -0,0 +1,21 @@
+using System;
+
+class DataTypes
+{
+    static void Main()
+    {
+        int num = 20;
+        float value = 49.99f;
+        double income = 45000.50;
+        char grade = 'A';
+        string name = "ABC";
+        bool isGood = true;
+
+        Console.WriteLine(num);
+        Console.WriteLine(value);
+        Console.WriteLine(income);
+        Console.WriteLine(grade);
+       Console.WriteLine(name);
+        Console.WriteLine(isGood);
+    }
+}
