0f897fab939aed537fde7215a93a3fa1ae5af25f
shivamGupta730 <144233987+shivamGupta730@users.noreply.github.com>
2025-12-19 11:52:28 +0530

    "[shivam gupta] datatypes"

--- core-csharp-practice/gcr-codebase/programming-elements/DataTpes.cs
+++ core-csharp-practice/gcr-codebase/programming-elements/DataTpes.cs
@@ -4,18 +4,31 @@ class DataTypes
 {
     static void Main()
     {
+        // Original variables
         int num = 20;
         float value = 49.99f;
         double income = 45000.50;
         char grade = 'A';
-        string name = "ABC";
-        bool isGood = true;
 
+        Console.WriteLine("Original Values:");
         Console.WriteLine(num);
         Console.WriteLine(value);
         Console.WriteLine(income);
         Console.WriteLine(grade);
-       Console.WriteLine(name);
-        Console.WriteLine(isGood);
+
+        // ---------------- Typecasting ----------------
+        Console.WriteLine("\nTypecasting Examples:");
+
+        float numFloat = (float)num;         // int to float
+        double numDouble = num;              // int to double (implicit)
+        int incomeInt = (int)income;         // double to int
+        int valueInt = (int)value;           // float to int
+        int gradeInt = (int)grade;           // char to int
+
+        Console.WriteLine("int to float: " + numFloat.ToString("0.0"));
+        Console.WriteLine("int to double: " + numDouble);
+        Console.WriteLine("double to int: " + incomeInt);
+        Console.WriteLine("float to int: " + valueInt);
+        Console.WriteLine("char to int: " + gradeInt);
     }
 }
