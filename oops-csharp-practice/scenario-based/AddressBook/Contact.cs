8b9a2ed3120452cf187a764bcef98a137344f0b8
shivamGupta730 <144233987+shivamGupta730@users.noreply.github.com>
2026-01-15 11:21:32 +0530

    "[shivam gupta]uc1 done"

---
+++ oops-csharp-practice/scenario-based/AddressBook/Contact.cs
@@ -0,0 +1,79 @@
+using System;
+
+namespace AddressBookSystem
+{
+    // UC1: Contact class to store person details
+    public class Contact
+    {
+        // Private variables (Encapsulation)
+        private string firstName;
+        private string lastName;
+        private string address;
+        private string city;
+        private string state;
+        private string zip;
+        private string phoneNumber;
+        private string email;
+
+        // Properties to access private variables
+        public string FirstName
+        {
+            get { return firstName; }
+            set { firstName = value; }
+        }
+
+        public string LastName
+        {
+            get { return lastName; }
+            set { lastName = value; }
+        }
+
+        public string Address
+        {
+            get { return address; }
+            set { address = value; }
+        }
+
+        public string City
+        {
+            get { return city; }
+            set { city = value; }
+        }
+
+        public string State
+        {
+            get { return state; }
+            set { state = value; }
+        }
+
+        public string Zip
+        {
+            get { return zip; }
+            set { zip = value; }
+        }
+
+        public string PhoneNumber
+        {
+            get { return phoneNumber; }
+            set { phoneNumber = value; }
+        }
+
+        public string Email
+        {
+            get { return email; }
+            set { email = value; }
+        }
+
+        // Used to display contact details
+        public override string ToString()
+        {
+            return "Name: " + FirstName + " " + LastName +
+                   "\nAddress: " + Address +
+                   "\nCity: " + City +
+                   "\nState: " + State +
+                   "\nZip: " + Zip +
+                   "\nPhone: " + PhoneNumber +
+                   "\nEmail: " + Email;
+        }
+    }
+}
