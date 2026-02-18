51fc75768b05800c0558e69e925a49b7a5cd6530
shivamGupta730 <144233987+shivamGupta730@users.noreply.github.com>
2026-02-18 16:53:15 +0530

    "[shivam gupta]commit"

--- oops-csharp-practice/scenario-based/AddressBook/Program.cs
+++ oops-csharp-practice/scenario-based/AddressBook/Program.cs
@@ -1,2 +1,16 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.Design;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace AddressBook
+{
+    public class Program
+    {
+        public static void Main()
+        {
+            new AddressBookMenu.ShowMenu();
+        }
+    }
+}
